66d2669ed481687a332fa340fd950ac6783822cb
Yinan Li <gabriel.li5566@gmail.com>
2022-08-19 12:25:09 +0300

    project init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import { Button, Toast } from 'antd-mobile'
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      App
+      <Button
+        color="primary"
+        onClick={() => {
+          Toast.show({
+            icon: 'loading',
+            content: 'Loading',
+          })
+        }}
+      >
+        Primary
+      </Button>
+    </div>
+  )
 }
