012fc7437262cd86cb54faf353e9f4d45ae7b629
Yinan Li <gabriel.li5566@gmail.com>
2022-08-25 00:23:46 +0300

    update Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,9 @@
-import React from 'react'
-import Icon from '@/components/Icon'
-import styles from './index.module.scss'
+import NavBar from '@/components/NavBar'
 
 export default function Login() {
   return (
-    <div className={styles.root}>
-      {/* Header */}
-      {/* 后退按钮 */}
-      <div className="left">
-        <Icon type="iconfanhui" />
-      </div>
-      {/* 居中标题 */}
-      <div className="title">我是标题</div>
-      {/* 右侧内容 */}
-      <div className="right">右侧内容</div>
+    <div>
+      <NavBar>Login</NavBar>
     </div>
   )
 }
