15ca47c958df0e2d9e894408d09a8467a6112030
Yinan Li <gabriel.li5566@gmail.com>
2022-08-25 16:05:48 +0300

    update login page scss

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,20 @@
 import NavBar from '@/components/NavBar'
+import styles from './index.module.scss'
 
 export default function Login() {
   return (
-    <div>
+    <div className={styles.root}>
       <NavBar>Login</NavBar>
+      <div className="content">
+        <h3>Sign in</h3>
+        <form>
+          <input type="text" />
+          <input type="text" />
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
