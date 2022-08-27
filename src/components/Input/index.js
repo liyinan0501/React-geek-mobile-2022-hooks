85257c006771b6d2a0374c59be8e04a4a011a6dc
Yinan Li <gabriel.li5566@gmail.com>
2022-08-27 20:15:52 +0300

    create Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export default function Input({ extra, onExtraClick, ...rest }) {
+  return (
+    <div className={styles.root}>
+      <input className="input" type="text" {...rest} />
+      {extra && (
+        <div className="extra" onClick={onExtraClick}>
+          {extra}
+        </div>
+      )}
+    </div>
+  )
+}
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,30 @@
 import NavBar from '@/components/NavBar'
 import styles from './index.module.scss'
+import Input from '@/components/Input'
 
 export default function Login() {
+  const onExtraClick = () => {
+    console.log('send')
+  }
+
   return (
     <div className={styles.root}>
       <NavBar>Login</NavBar>
       <div className="content">
         <h3>Sign in</h3>
         <form>
-          <input type="text" />
-          <input type="text" />
+          <div className="input-item">
+            <Input placeholder="Phone Number" />
+            {/* <div className="validate">phone number validation</div> */}
+          </div>
+          <div className="input-item">
+            <Input
+              placeholder="Code"
+              extra="Send"
+              onExtraClick={onExtraClick}
+            />
+            {/* <div className="validate">code validation</div> */}
+          </div>
           <button type="submit" className="login-btn">
             Login
           </button>
