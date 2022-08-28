883150ae09e19871c8ef61f8b397cd6e2188e53b
Yinan Li <gabriel.li5566@gmail.com>
2022-08-28 15:31:08 +0300

    update Input Component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -5,11 +5,11 @@ export default function Input({ extra, onExtraClick, ...rest }) {
   return (
     <div className={styles.root}>
       <input className="input" type="text" {...rest} />
-      {extra && (
+      {extra ? (
         <div className="extra" onClick={onExtraClick}>
           {extra}
         </div>
-      )}
+      ) : null}
     </div>
   )
 }
