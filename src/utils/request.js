cc4de66813aa4dc96cd8a32880d59ce6992d9cb5
Yinan Li <gabriel.li5566@gmail.com>
2022-09-04 14:28:13 +0300

    config basic axios

---
+++ src/utils/request.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export const baseURL = 'http://geek.itheima.net/v1_0/'
+
+const request = axios.create({
+  timeout: 5000,
+  baseURL,
+})
+
+// Request interceptor
+axios.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return config
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// Response interceptor
+axios.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
+
+export default request
